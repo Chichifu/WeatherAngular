2eb71d301bfc47cbf0999990812d1026eab6615d
Chichifu <manuel.couseloalonso@gmail.com>
2019-04-06 02:44:11 +0200

    Batman Weather

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {WeatherService} from './service/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Ejercicio1Frontend';
+export class AppComponent implements OnInit {
+
+  weather;
+
+  constructor(private weatherService: WeatherService) {
+
+  }
+
+  ngOnInit() {
+    this.weatherService.getWeather(`Vigo`, `spa`).subscribe(
+      res => console.log(res),
+      err => console.log(err)
+    );
+  }
+
+  getWeather(ciudad: string, pais: string) {
+    this.weatherService.getWeather(ciudad, pais).subscribe(
+      res => {
+        console.log(this.weather);
+        this.weather = res;
+      },
+      err => console.log(err)
+    );
+  }
+
+  subbmitLocation(ciudad: HTMLInputElement, pais: HTMLInputElement) {
+    if (ciudad.value && pais.value) {
+      this.getWeather(ciudad.value, pais.value);
+      ciudad.value = '';
+      pais.value = '';
+    } else {
+      alert('Inserte datos');
+    }
+    ciudad.focus();
+    return false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
